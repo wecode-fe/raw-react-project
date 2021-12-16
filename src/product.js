c7670d90cc988e255c2ee4f2b15e716666c47795
Mohammed Maher <mohammed.m.salloom@gmail.com>
2021-12-16 15:55:41 +0300

    configured prettier

--- src/product.js
+++ src/product.js
@@ -1,10 +1,16 @@
-const Product = props =>{
-    return React.createElement("div",{className:'product'},
-        React.createElement("h1",{},props.title),
-        React.createElement("img", {src:props.image}),
-        React.createElement("p", {}, `${props.price} IQD`),
-        React.createElement("button",{
-            onClick:()=>alert('You clicked buy now on product'+ props.title)
-        },"BUY NOW")
+const Product = (props) => {
+  return React.createElement(
+    "div",
+    { className: "product" },
+    React.createElement("h1", {}, props.title),
+    React.createElement("img", { src: props.image }),
+    React.createElement("p", {}, `${props.price} IQD`),
+    React.createElement(
+      "button",
+      {
+        onClick: () => alert("You clicked buy now on product" + props.title),
+      },
+      "BUY NOW"
     )
-}
+  );
+};
